8eeae6e27d5f4853199f4ef575f6774438bc0176
PratikshaJoshi2021 <pratikshapjoshi16@gmail.com>
2022-05-24 19:18:05 +0530

    added the remaining projects

---
+++ ConsoleApp15/ConsoleApp15/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, temp;
+            int[] arr = new int[5];
+            Console.WriteLine("Enter five numbers:");
+            for(i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+                 
+            }
+            for(i=0; i<5; i++)
+            {
+                for(j=i+1;j<5;j++)
+                {
+                    if(arr[i]>arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Second Largest Element is:" + arr[5 - 2]);
+        }
+    }
+}
---
+++ ConsoleApp16/ConsoleApp16/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 3, 4, 2 };
+            Console.WriteLine("Duplicate array elements are:");
+            for(int i=0; i<arr.Length;i++)
+            {
+                for(int j=i+1; j<arr.Length;j++)
+                {
+                    if (arr[i] == arr[j])
+                        Console.WriteLine(arr[j]);
+                }
+            }
+        }
+    }
+}
