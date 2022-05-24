e38b5205b1acdfa6cd06fd8ce9a7c90b479ce806
PratikshaJoshi2021 <pratikshapjoshi16@gmail.com>
2022-05-24 12:25:05 +0530

    changes

---
+++ ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int row = 0; row <= 9; row++)
+            {
+                for(int col = 0; col <= 9; col++)
+                {
+                    if(row == 0 || col == 0 || row == 9 || col == 9 || row == col || col==9-row+1)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
